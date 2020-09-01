c0abea659895e624f7cf6bbd2aeb075e48fa8054
Kumachan <y.s11nov@fuji.waseda.jp>
2020-09-02 00:35:11 +0900

    fix: cursor remains even after scrolling (#10)

--- dist/Cursor/index.d.ts
+++ dist/Cursor/index.d.ts
@@ -1,8 +1,11 @@
 import * as React from "react";
 import { Props, State } from "./types";
 export declare class Cursor extends React.Component<Props, State> {
+    private root;
     private textArea;
+    private handleOnEditorScroll;
     constructor(props: Props);
+    componentDidMount(): void;
     componentDidUpdate(prevProps: Readonly<Props>): void;
     render(): JSX.Element;
 }
--- src/Cursor/index.tsx
+++ src/Cursor/index.tsx
@@ -2,29 +2,49 @@ import * as React from "react";
 
 import { Props, State } from "./types";
 import { CursorConstants } from "./constants";
-import { cursorPropsToState } from "./utils";
+import { cursorPropsToState, handleOnEditorScroll, cursorIn } from "./utils";
+
+import { TextLinesConstants } from "../TextLines/constants";
 
 export class Cursor extends React.Component<Props, State> {
+  private root: HTMLElement | null;
   private textArea: HTMLTextAreaElement | null;
+  private handleOnEditorScroll: () => void;
 
   constructor(props: Props) {
     super(props);
     this.state = { position: [0, 0], textAreaValue: "", cursorSize: 0 };
+    this.root = null;
     this.textArea = null;
+    this.handleOnEditorScroll = () => {
+      /* do nothing */
+    };
+  }
+
+  componentDidMount(): void {
+    this.root = document.getElementById(TextLinesConstants.id);
   }
 
   componentDidUpdate(prevProps: Readonly<Props>): void {
     if (prevProps == this.props) return;
     const state = cursorPropsToState(this.props, this.state);
-    this.setState(state);
+    if (state != this.state) this.setState(state);
     if (this.props.coordinate) this.textArea?.focus();
+    if (this.root) {
+      this.root.removeEventListener("scroll", this.handleOnEditorScroll);
+      this.handleOnEditorScroll = () => {
+        const state = handleOnEditorScroll(this.props, this.state);
+        if (state != this.state) this.setState(state);
+      };
+      this.root.addEventListener("scroll", this.handleOnEditorScroll);
+    }
   }
 
   render(): JSX.Element {
     const [top, left] = this.state.position;
-    const hidden = this.state.position === undefined;
-    const textLength = this.state.textAreaValue.length;
     const { cursorSize } = this.state;
+    const hidden = this.state.position === undefined || !cursorIn([top, left], cursorSize);
+    const textLength = this.state.textAreaValue.length;
     return (
       <>
         <div style={CursorConstants.rootDiv.style(top, left, cursorSize, hidden)}>
