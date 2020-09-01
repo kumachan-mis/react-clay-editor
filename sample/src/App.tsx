c0becea0c802253d56318c704e323bb4407bbdaf
kumachan-mis <y.s11nov@fuji.waseda.jp>
2020-09-01 21:00:10 +0900

    default text

--- sample/src/App.tsx
+++ sample/src/App.tsx
@@ -2,7 +2,28 @@ import * as React from "react";
 
 import { Editor } from "../../src";
 
+const defaultText = [
+  "[*** Realtime Markup Editor Example]",
+  "A text document editor which is syntactically formattable in place",
+  "",
+  " can use itemizations",
+  " by just typing a space",
+  " like this",
+  "  can also use nested itemizations",
+  "  by just typing multiple spaces",
+  "  like this",
+  "",
+  "[** Some text decorations are supported]",
+  " [* bold]",
+  " [/ italic]",
+  " [_ underline]",
+  "",
+  "can combine these text decorations",
+  " [*/ bold italic]",
+  " [*_ bold underline]",
+].join("\n");
+
 export const App: React.FC = () => {
-  const [text, setText] = React.useState("sample text");
+  const [text, setText] = React.useState(defaultText);
   return <Editor text={text} onChangeText={setText} style={{ width: "800px", height: "500px" }} />;
 };
