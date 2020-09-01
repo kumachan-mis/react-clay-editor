0b84ee1f3bde01cd57c793202070c8cfc8990396
kumachan-mis <y.s11nov@fuji.waseda.jp>
2020-09-01 21:04:29 +0900

    fix

--- sample/src/App.tsx
+++ sample/src/App.tsx
@@ -3,14 +3,14 @@ import * as React from "react";
 import { Editor } from "../../src";
 
 const defaultText = [
-  "[*** Realtime Markup Editor Example]",
+  "[*** Realtime Markup Editor]",
   "[** What's this?]",
   "A text document editor which is syntactically formattable in place",
   "",
-  " can use itemizations",
+  " You can use itemizations",
   " by just typing a space",
   " like this",
-  "  can also use nested itemizations",
+  "  You can also use nested itemizations",
   "  by just typing multiple spaces",
   "  like this",
   "",
@@ -19,10 +19,11 @@ const defaultText = [
   " [/ italic]",
   " [_ underline]",
   "",
-  "can combine these text decorations",
+  "You can combine these text decorations",
   " [*/ bold italic]",
   " [*_ bold underline]",
   "and so on...",
+  "",
 ].join("\n");
 
 export const App: React.FC = () => {
