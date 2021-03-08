628538cd7816ea65b559b43284c0aebbadc16aca
kumachan-mis <y.s11nov@fuji.waseda.jp>
2021-03-08 13:03:57 +0900

    pass tests

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@ const path = require('path');
 module.exports = {
   preset: 'ts-jest',
   moduleFileExtensions: ['ts', 'js', 'tsx', 'jsx'],
+  moduleNameMapper: {
+    '\\.(css|less)$': '<rootDir>/tests/stylemock.ts',
+  },
   setupFilesAfterEnv: [path.resolve(__dirname, 'tests', 'jest.setup.ts')],
   verbose: true,
 };
