7bf9cb8d63183e42830e9c38f58b505707cc597e
kumachan-mis <y.s11nov@fuji.waseda.jp>
2021-06-18 09:31:20 +0900

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -8,8 +8,7 @@ export {
   CodeProps,
   FormulaProps,
 } from './Editor/types';
-export { defaultSuggestionListDecoration
- } from './Cursor/constants';
+export { defaultSuggestionListDecoration } from './Cursor/constants';
 export {
   defaultTextDecoration,
   defaultLinkNameRegex,
