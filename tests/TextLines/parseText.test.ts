f2343b06891973c129d38daa3c33c1a3a27689fe
Kumachan <y.s11nov@fuji.waseda.jp>
2021-10-17 16:02:45 +0900

    test: keyboard operation test (#187)

--- tests/TextLines/parseText.test.ts
+++ tests/TextLines/parseText.test.ts
@@ -7,19 +7,36 @@ interface TestCase extends BaseTestCase {
   name: string;
   inputLines: string[];
   expectedNodes: Node[];
+  options?: Omit<ParsingOptions, 'syntax'>;
 }
 
-function createTest(syntax: 'bracket' | 'markdown'): (testCase: TestCase) => void {
-  return (testCase) => {
+interface Common {
+  options?: Omit<ParsingOptions, 'syntax'>;
+}
+
+function createTest(syntax: 'bracket' | 'markdown'): (testCase: TestCase, common?: Common) => void {
+  return (testCase, common) => {
     const options: ParsingOptions = {
       disabledMap: { bracketLink: false, hashTag: false, code: false, formula: false },
       taggedLinkRegexes: [],
       syntax,
+      ...common?.options,
+      ...testCase.options,
     };
     const actualNodes = parseText(testCase.inputLines.join('\n'), options);
     expect(actualNodes).toMatchObject(testCase.expectedNodes);
   };
 }
 
-fixtureTest<TestCase>('parseText', 'TextLines', ['parseCommonText', 'parseBracketText'], createTest('bracket'));
-fixtureTest<TestCase>('parseText', 'TextLines', ['parseCommonText', 'parseMarkdownText'], createTest('markdown'));
+fixtureTest<TestCase, Common | undefined>(
+  'parseText',
+  'TextLines',
+  ['parseCommonText', 'parseBracketText'],
+  createTest('bracket')
+);
+fixtureTest<TestCase, Common | undefined>(
+  'parseText',
+  'TextLines',
+  ['parseCommonText', 'parseMarkdownText'],
+  createTest('markdown')
+);
