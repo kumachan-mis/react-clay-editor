20bf390381ae5a571ffd3e24121a5db44bec00f9
kumachan-mis <y.s11nov@fuji.waseda.jp>
2021-10-18 15:17:38 +0900

    .

--- tests/Selection/getWordSelection.test.ts
+++ tests/Selection/getWordSelection.test.ts
@@ -1,17 +1,21 @@
 import { fixtureTest, BaseTestCase } from '../utils/fixtureTest';
 
-import { getWordSelection } from '../../src/Selection/utils';
-import { TextSelection } from '../../src/Selection/types';
+import { getSelectedText, getWordSelection } from '../../src/Selection/utils';
 import { CursorCoordinate } from '../../src/Cursor/types';
 
 interface TestCase extends BaseTestCase {
   name: string;
-  inputLines: string[];
   inputCursorCoordinate: CursorCoordinate | undefined;
-  expectedSelection: TextSelection | undefined;
+  expectedText: string;
 }
 
-fixtureTest<TestCase>('getWordSelection', 'Selection', 'getWordSelection', (testCase) => {
-  const actualCursorCoordinate = getWordSelection(testCase.inputLines.join('\n'), testCase.inputCursorCoordinate);
-  expect(actualCursorCoordinate).toEqual(testCase.expectedSelection);
+interface Common {
+  inputLines: string[];
+}
+
+fixtureTest<TestCase, Common>('getWordSelection', 'Selection', 'getWordSelection', (testCase, common) => {
+  const text = common.inputLines.join('\n');
+  const actualSelection = getWordSelection(text, testCase.inputCursorCoordinate);
+  const actualText = getSelectedText(text, actualSelection);
+  expect(actualText).toEqual(testCase.expectedText);
 });
