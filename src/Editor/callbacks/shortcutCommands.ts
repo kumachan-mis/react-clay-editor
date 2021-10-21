f86372cf9783cc25d4b5a9a40a84847e46f1edbc
kumachan-mis <y.s11nov@fuji.waseda.jp>
2021-10-21 21:54:35 +0900

    .

--- src/Editor/callbacks/shortcutCommands.ts
+++ src/Editor/callbacks/shortcutCommands.ts
@@ -3,7 +3,6 @@ import { isMacOS } from '../../common/utils';
 
 export function shortcutCommand(event: React.KeyboardEvent<HTMLTextAreaElement>): ShortcutCommand | undefined {
   if (forwardDeleteTriggered(event)) return 'forwardDelete';
-  if (backwardDeleteTriggered(event)) return 'backwardDelete';
   if (selectAllTriggered(event)) return 'selectAll';
   if (undoTriggered(event)) return 'undo';
   if (redoTriggered(event)) return 'redo';
@@ -11,22 +10,21 @@ export function shortcutCommand(event: React.KeyboardEvent<HTMLTextAreaElement>)
   if (moveDownTriggered(event)) return 'moveDown';
   if (moveLeftTriggered(event)) return 'moveLeft';
   if (moveRightTriggered(event)) return 'moveRight';
-  if (moveWordTopTriggered(event)) return 'moveWordTop';
-  if (moveWordBottomTriggered(event)) return 'moveWordBottom';
   if (moveLineTopTriggered(event)) return 'moveLineTop';
   if (moveLineBottomTriggered(event)) return 'moveLineBottom';
-  if (moveTextTopTriggered(event)) return 'moveTextTop';
-  if (moveTextBottomTriggered(event)) return 'moveTextBottom';
+
+  // any shortcut commands with a-z are not defined for the following functions
+  // - backwardDelete
+  // - moveWordTop
+  // - moveWordBottom
+  // - moveTextTop
+  // - moveTextBottom
+
   return undefined;
 }
 
 function forwardDeleteTriggered(event: React.KeyboardEvent<HTMLTextAreaElement>): boolean {
   return isMacOS() && event.key == 'd' && event.ctrlKey && !event.metaKey && !event.altKey && !event.shiftKey;
-}
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-function backwardDeleteTriggered(event: React.KeyboardEvent<HTMLTextAreaElement>): boolean {
-  return false;
 }
 
 function selectAllTriggered(event: React.KeyboardEvent<HTMLTextAreaElement>): boolean {
@@ -71,30 +69,10 @@ function moveRightTriggered(event: React.KeyboardEvent<HTMLTextAreaElement>): bo
   return event.ctrlKey && (event.key == 'f' || event.key == 'F') && !event.metaKey && !event.altKey;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-function moveWordTopTriggered(event: React.KeyboardEvent<HTMLTextAreaElement>): boolean {
-  return false;
-}
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-function moveWordBottomTriggered(event: React.KeyboardEvent<HTMLTextAreaElement>): boolean {
-  return false;
-}
-
 function moveLineTopTriggered(event: React.KeyboardEvent<HTMLTextAreaElement>): boolean {
   return isMacOS() && event.ctrlKey && (event.key == 'a' || event.key == 'A') && !event.metaKey && !event.altKey;
 }
 
 function moveLineBottomTriggered(event: React.KeyboardEvent<HTMLTextAreaElement>): boolean {
   return isMacOS() && event.ctrlKey && (event.key == 'e' || event.key == 'E') && !event.metaKey && !event.altKey;
-}
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-function moveTextTopTriggered(event: React.KeyboardEvent<HTMLTextAreaElement>): boolean {
-  return false;
-}
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-function moveTextBottomTriggered(event: React.KeyboardEvent<HTMLTextAreaElement>): boolean {
-  return false;
 }
