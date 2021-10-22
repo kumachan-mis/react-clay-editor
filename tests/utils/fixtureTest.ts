7d757c564135339dce92f3d327454d85577e4e77
kumachan-mis <y.s11nov@fuji.waseda.jp>
2021-10-22 13:00:49 +0900

    .

--- tests/utils/fixtureTest.ts
+++ tests/utils/fixtureTest.ts
@@ -13,17 +13,13 @@ export interface TestFixture<TestCase extends BaseTestCase, Common> {
 export function fixtureTest<TestCase extends BaseTestCase, Common = undefined>(
   target: string,
   component: string,
-  fixtureName: string | string[],
+  fixtureName: string,
   testfn: (testCase: TestCase, common: Common) => void
 ): void {
   describe(`Unit test of ${target} in ${component}: ${fixtureName}`, () => {
     const fixturesDirPath = resolve(__dirname, '..', '..', 'test-fixtures');
-    const fixtureNames = typeof fixtureName === 'string' ? [fixtureName] : fixtureName;
-
-    for (const fixtureName of fixtureNames) {
-      const fixturesFilePath = resolve(fixturesDirPath, component, `${fixtureName}.json`);
-      const fixtures = JSON.parse(readFileSync(fixturesFilePath, 'utf-8')) as TestFixture<TestCase, Common>;
-      fixtures.testCases.forEach((testCase) => test(testCase.name, () => testfn(testCase, fixtures.common)));
-    }
+    const fixturesFilePath = resolve(fixturesDirPath, component, `${fixtureName}.json`);
+    const fixtures = JSON.parse(readFileSync(fixturesFilePath, 'utf-8')) as TestFixture<TestCase, Common>;
+    fixtures.testCases.forEach((testCase) => test(testCase.name, () => testfn(testCase, fixtures.common)));
   });
 }
