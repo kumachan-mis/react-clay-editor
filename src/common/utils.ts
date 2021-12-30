2e2e02a25f5e6f7b9b47ce2eb7efdd1ab54c4ad2
kumachan-mis <y.s11nov@fuji.waseda.jp>
2021-12-30 13:08:51 +0900

    fix: remove data-testid from production code

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,5 +1,3 @@
-import { SelectIdProps } from './types';
-
 export function isMacOS(): boolean {
   return navigator.userAgent.indexOf('Mac OS X') > -1;
 }
@@ -8,6 +6,6 @@ export function mergeClassNames(...classNames: (string | undefined)[]): string {
   return classNames.filter((className) => !!className).join(' ');
 }
 
-export function selectIdProps(selectId: string): SelectIdProps {
-  return { 'data-selectid': selectId, 'data-testid': selectId };
+export function selectIdProps(selectId: string): { 'data-selectid': string } {
+  return { 'data-selectid': selectId };
 }
--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -1,1 +1,16 @@
 import '@testing-library/jest-dom';
+import * as utils from '../src/common/utils';
+
+const spiedSelectIdProps = jest.spyOn(utils, 'selectIdProps');
+
+beforeAll(() => {
+  spiedSelectIdProps.mockImplementation((selectId) => ({ 'data-selectid': selectId, 'data-testid': selectId }));
+});
+
+afterAll(() => {
+  spiedSelectIdProps.mockRestore();
+});
+
+afterEach(() => {
+  spiedSelectIdProps.mockClear();
+});
