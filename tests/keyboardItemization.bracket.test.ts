c9f26f616d0486fc8d3cd7642beac434b7cf48c7
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2022-10-23 15:50:00 +0900

    test: use playwright in keyboard itemization test

---
+++ tests/keyboardItemization.bracket.test.ts
@@ -0,0 +1,77 @@
+import test from '@playwright/test';
+
+import { linesToBe } from './testUtils';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('.');
+  await page.locator('[data-selectid=editor-body]').click();
+});
+
+test('itemization enter (empty next)', async ({ page }) => {
+  await page.keyboard.press('Space');
+  await page.keyboard.type('item');
+  await page.keyboard.press('Space');
+  await page.keyboard.type('1');
+  await page.keyboard.press('Enter');
+
+  await page.keyboard.type('item');
+  await page.keyboard.press('Space');
+  await page.keyboard.type('2');
+  await page.keyboard.press('Enter');
+
+  await page.keyboard.press('Space');
+  await page.keyboard.type('item');
+  await page.keyboard.press('Space');
+  await page.keyboard.type('3');
+  await page.keyboard.press('Enter');
+
+  await page.keyboard.type('item');
+  await page.keyboard.press('Space');
+  await page.keyboard.type('4');
+
+  await linesToBe(page, [' item 1', ' item 2', '  item 3', '  item 4']);
+});
+
+test('itemization enter (non-empty next)', async ({ page }) => {
+  await page.keyboard.press('Space');
+  await page.keyboard.type('xx');
+  await page.keyboard.press('ArrowLeft');
+  await page.keyboard.press('Enter');
+  await page.keyboard.press('ArrowRight');
+  await page.keyboard.press('Enter');
+
+  await page.keyboard.press('Space');
+  await page.keyboard.type('yy');
+  await page.keyboard.press('ArrowLeft');
+  await page.keyboard.press('Enter');
+  await page.keyboard.press('ArrowRight');
+
+  await linesToBe(page, [' x', ' x', '  y', '  y']);
+});
+
+test('non-itemization enter', async ({ page }) => {
+  await page.keyboard.type('-');
+  await page.keyboard.press('Space');
+  await page.keyboard.type('item');
+  await page.keyboard.press('Space');
+  await page.keyboard.type('1');
+  await page.keyboard.press('Enter');
+
+  await page.keyboard.type('item');
+  await page.keyboard.press('Space');
+  await page.keyboard.type('2');
+
+  await linesToBe(page, ['- item 1', 'item 2']);
+});
+
+test('itemization backspace', async ({ page }) => {
+  await page.keyboard.press('Space');
+  await page.keyboard.type('i');
+
+  await page.keyboard.press('Backspace');
+  await page.keyboard.press('Backspace');
+
+  await page.keyboard.type('.');
+
+  await linesToBe(page, ['.']);
+});
