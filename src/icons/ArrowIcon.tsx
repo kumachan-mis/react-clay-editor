a1f63e9b28e733ddfe5998685431c6668e8a2d99
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2022-10-30 09:05:08 +0900

    icons

---
+++ src/icons/ArrowIcon.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { SVGProps } from 'react';
+
+const SvgArrow = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000" xmlSpace="preserve" {...props}>
+    <path d="M500 765.3 10 260.8l26.9-26.1L500 711.5l463.1-476.8 26.9 26.1z" />
+  </svg>
+);
+export default SvgArrow;
