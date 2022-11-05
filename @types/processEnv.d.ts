1495fe5e2ebb1576abdcd0cf1bda0f42737d0f0a
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2022-11-06 02:01:02 +0900

    remove config for css files

--- @types/processEnv.d.ts
+++
@@ -1,5 +0,0 @@
-declare namespace NodeJS {
-  export type ProcessEnv = {
-    ENVIRONMENT: string;
-  };
-}
--- @types/styles.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '*.css' {
-  type IClassNames = {
-    [className: string]: string;
-  };
-  const classNames: IClassNames;
-  export default classNames;
-}
