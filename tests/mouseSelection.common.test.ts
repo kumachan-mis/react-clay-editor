620e1881a77f0938d539096e682a6b8af23271b9
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2022-12-04 16:43:32 +0900

    mouse selection test

---
+++ tests/mouseSelection.common.test.ts
@@ -0,0 +1,115 @@
+import test, { Page } from '@playwright/test';
+
+import { linesToBe } from './testUtils';
+
+test.beforeEach(async ({ page }) => {
+  const defaultText = [
+    'Genius is one percent inspiration and',
+    'ninety-nine percent perspiration',
+    'by Thomas Edison',
+  ].join('\n');
+
+  await page.goto('.');
+  await page.locator('[data-selectid=text-field]').click();
+  await page.keyboard.insertText(defaultText);
+});
+
+test('one line selection', async ({ page }) => {
+  await mouseSelect(page, '[data-selectid=char-L0C1]', '[data-selectid=char-L0C15]');
+  await page.keyboard.type('.');
+
+  await linesToBe(page, [
+    // expected lines
+    'G.ercent inspiration and',
+    'ninety-nine percent perspiration',
+    'by Thomas Edison',
+  ]);
+});
+
+test('two lines selection', async ({ page }) => {
+  await mouseSelect(page, '[data-selectid=char-L1C25]', '[data-selectid=char-L2C5]');
+  await page.keyboard.type('.');
+
+  await linesToBe(page, [
+    // expected lines
+    'Genius is one percent inspiration and',
+    'ninety-nine percent persp.omas Edison',
+  ]);
+});
+
+test('three lines selection', async ({ page }) => {
+  await mouseSelect(page, '[data-selectid=char-L0C29]', '[data-selectid=char-L2C10]');
+  await page.keyboard.type('.');
+
+  await linesToBe(page, [
+    // expected lines
+    'Genius is one percent inspira.Edison',
+  ]);
+});
+
+test('double click selection', async ({ page }) => {
+  await page.locator('[data-selectid=char-L2C15]').click({ clickCount: 2 });
+  await page.keyboard.type('.');
+
+  await linesToBe(page, [
+    // expected lines
+    'Genius is one percent inspiration and',
+    'ninety-nine percent perspiration',
+    'by Thomas .',
+  ]);
+});
+
+test('triple click selection', async ({ page }) => {
+  await page.locator('[data-selectid=char-L2C15]').click({ clickCount: 3 });
+  await page.keyboard.type('.');
+
+  await linesToBe(page, [
+    // expected lines
+    'Genius is one percent inspiration and',
+    'ninety-nine percent perspiration',
+    '.',
+  ]);
+});
+
+test('"down then up without move', async ({ page }) => {
+  await mouseSelect(page, '[data-selectid=char-L2C15]', '[data-selectid=char-L2C15]');
+  await page.keyboard.type('.');
+
+  await linesToBe(page, [
+    'Genius is one percent inspiration and',
+    'ninety-nine percent perspiration',
+    'by Thomas Ediso.n',
+  ]);
+});
+
+test('move without down and up', async ({ page }) => {
+  await mouseMove(page, '[data-selectid=char-L1C25]', '[data-selectid=char-L2C5]');
+  await page.keyboard.type('.');
+
+  await linesToBe(page, [
+    // expected lines
+    'Genius is one percent inspiration and',
+    'ninety-nine percent perspiration',
+    'by Thomas Edison.',
+  ]);
+});
+
+async function mouseSelect(page: Page, fixedSelector: string, freeSelector: string) {
+  const fixedBoundingBox = await page.locator(fixedSelector).boundingBox();
+  const freeBoundingBox = await page.locator(freeSelector).boundingBox();
+  if (!fixedBoundingBox || !freeBoundingBox) return;
+
+  await page.mouse.move(fixedBoundingBox.x, fixedBoundingBox.y + fixedBoundingBox.height / 2);
+  await page.mouse.down();
+  await page.mouse.move(freeBoundingBox.x, freeBoundingBox.y + freeBoundingBox.height / 2, { steps: 5 });
+  await page.mouse.up();
+}
+
+async function mouseMove(page: Page, fixedSelector: string, freeSelector: string) {
+  const fixedBoundingBox = await page.locator(fixedSelector).boundingBox();
+  const freeBoundingBox = await page.locator(freeSelector).boundingBox();
+  if (!fixedBoundingBox || !freeBoundingBox) return;
+
+  await page.mouse.move(fixedBoundingBox.x, fixedBoundingBox.y + fixedBoundingBox.height / 2);
+  await page.mouse.move(freeBoundingBox.x, freeBoundingBox.y + freeBoundingBox.height / 2, { steps: 5 });
+}
