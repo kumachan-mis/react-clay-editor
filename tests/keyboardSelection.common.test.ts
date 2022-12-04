d1ead048d5b363dea74b41458b9112118e215bbe
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2022-12-04 15:58:45 +0900

    keyboard selection test

---
+++ tests/keyboardSelection.common.test.ts
@@ -0,0 +1,117 @@
+import test from '@playwright/test';
+
+import { linesToBe } from './testUtils';
+
+test.beforeEach(async ({ page }) => {
+  const defaultText = [
+    'Genius is one percent inspiration and',
+    'ninety-nine percent perspiration',
+    'by Thomas Edison',
+  ].join('\n');
+
+  await page.goto('.');
+  await page.locator('[data-selectid=text-field]').click();
+  await page.keyboard.insertText(defaultText);
+});
+
+test('select on move left', async ({ page }) => {
+  await page.locator('[data-selectid=char-L1C17]').click();
+
+  await page.keyboard.down('Shift');
+  await page.keyboard.press('ArrowLeft');
+  await page.keyboard.press('ArrowLeft');
+  await page.keyboard.up('Shift');
+
+  await page.keyboard.type('.');
+
+  await linesToBe(page, [
+    // expected lines
+    'Genius is one percent inspiration and',
+    'ninety-nine per.nt perspiration',
+    'by Thomas Edison',
+  ]);
+});
+
+test('select on move right', async ({ page }) => {
+  await page.locator('[data-selectid=char-L1C17]').click();
+
+  await page.keyboard.down('Shift');
+  await page.keyboard.press('ArrowRight');
+  await page.keyboard.press('ArrowRight');
+  await page.keyboard.up('Shift');
+
+  await page.keyboard.type('.');
+
+  await linesToBe(page, [
+    // expected lines
+    'Genius is one percent inspiration and',
+    'ninety-nine perce. perspiration',
+    'by Thomas Edison',
+  ]);
+});
+
+test('select on move up', async ({ page }) => {
+  await page.locator('[data-selectid=char-L1C17]').click();
+
+  await page.keyboard.down('Shift');
+  await page.keyboard.press('ArrowUp');
+  await page.keyboard.up('Shift');
+
+  await page.keyboard.type('.');
+
+  await linesToBe(page, [
+    // expected lines
+    'Genius is one per.nt perspiration',
+    'by Thomas Edison',
+  ]);
+});
+
+test('select on move down', async ({ page }) => {
+  await page.locator('[data-selectid=char-L1C17]').click();
+
+  await page.keyboard.down('Shift');
+  await page.keyboard.press('ArrowDown');
+  await page.keyboard.up('Shift');
+
+  await page.keyboard.type('.');
+
+  await linesToBe(page, [
+    // expected lines
+    'Genius is one percent inspiration and',
+    'ninety-nine perce.',
+  ]);
+});
+
+test('select on move line top', async ({ page }) => {
+  await page.locator('[data-selectid=char-L1C17]').click();
+
+  await page.keyboard.down('Shift');
+  await page.keyboard.press('Home');
+  await page.keyboard.up('Shift');
+
+  await page.keyboard.type('.');
+
+  await linesToBe(page, [
+    // expected lines
+    'Genius is one percent inspiration and',
+    '.nt perspiration',
+    'by Thomas Edison',
+  ]);
+});
+
+test('select on move line bottom', async ({ page }) => {
+  await page.locator('[data-selectid=char-L1C17]').click();
+
+  await page.keyboard.down('Shift');
+  await page.keyboard.press('End');
+  await page.keyboard.up('Shift');
+
+  await page.keyboard.type('.');
+
+  await linesToBe(page, [
+    // expected lines
+    'Genius is one percent inspiration and',
+    'ninety-nine perce.',
+    'by Thomas Edison',
+  ]);
+});
---
+++ tests/keyboardSelection.windows.test.ts
@@ -0,0 +1,81 @@
+import test from '@playwright/test';
+
+import { linesToBe } from './testUtils';
+
+test.beforeEach(async ({ page }) => {
+  const defaultText = [
+    'Genius is one percent inspiration and',
+    'ninety-nine percent perspiration',
+    'by Thomas Edison',
+  ].join('\n');
+
+  await page.goto('.');
+  await page.locator('[data-selectid=text-field]').click();
+  await page.keyboard.insertText(defaultText);
+});
+
+test('select on word top', async ({ page }) => {
+  await page.locator('[data-selectid=char-L1C17]').click();
+
+  await page.keyboard.down('Shift');
+  await page.keyboard.press('Control+ArrowLeft');
+  await page.keyboard.up('Shift');
+
+  await page.keyboard.type('.');
+
+  await linesToBe(page, [
+    // expected lines
+    'Genius is one percent inspiration and',
+    'ninety-nine .nt perspiration',
+    'by Thomas Edison',
+  ]);
+});
+
+test('select on word bottom', async ({ page }) => {
+  await page.locator('[data-selectid=char-L1C17]').click();
+
+  await page.keyboard.down('Shift');
+  await page.keyboard.press('Control+ArrowRight');
+  await page.keyboard.up('Shift');
+
+  await page.keyboard.type('.');
+
+  await linesToBe(page, [
+    // expected lines
+    'Genius is one percent inspiration and',
+    'ninety-nine perce. perspiration',
+    'by Thomas Edison',
+  ]);
+});
+
+test('select on text top', async ({ page }) => {
+  await page.locator('[data-selectid=char-L1C17]').click();
+
+  await page.keyboard.down('Shift');
+  await page.keyboard.press('Control+Home');
+  await page.keyboard.up('Shift');
+
+  await page.keyboard.type('.');
+
+  await linesToBe(page, [
+    // expected lines
+    '.nt perspiration',
+    'by Thomas Edison',
+  ]);
+});
+
+test('select on text bottom', async ({ page }) => {
+  await page.locator('[data-selectid=char-L1C17]').click();
+
+  await page.keyboard.down('Shift');
+  await page.keyboard.press('Control+End');
+  await page.keyboard.up('Shift');
+
+  await page.keyboard.type('.');
+
+  await linesToBe(page, [
+    // expected lines
+    'Genius is one percent inspiration and',
+    'ninety-nine perce.',
+  ]);
+});
