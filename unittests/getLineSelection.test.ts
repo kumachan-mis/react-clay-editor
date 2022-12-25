ba69ae9f36821a777931e205e465711ce1387f26
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2022-12-25 18:20:57 +0900

    update jest.config.js

--- unittests/getLineSelection.test.ts
+++ unittests/getLineSelection.test.ts
@@ -1,6 +1,8 @@
-import { expect, describe, test } from '@jest/globals';
 import { getLineSelection } from 'src/components/molecules/selection/Selection/utils';
-import { TestCase, testCases } from './cursorCoordinateToTextIndex.testcase';
+
+import { TestCase, testCases } from './getLineSelection.testcase';
+
+import { expect, describe, test } from '@jest/globals';
 
 describe('function getLineSelection', () => {
   test.each<[string, TestCase]>(testCases.map((testCase) => [testCase.name, testCase]))('%s', (_, testCase) => {
