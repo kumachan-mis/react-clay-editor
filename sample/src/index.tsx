b4a19944d2a1f2e8c6b96b77e94da8cf902ef9fb
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2022-12-28 23:27:40 +0900

    delete sample

--- sample/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import { App } from './App';
-
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-
-const container = document.getElementById('app');
-const root = createRoot(container as HTMLElement);
-root.render(<App />);
