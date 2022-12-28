388dded5672d13f5f4094a6fdacff67dbc464d7d
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2022-12-29 00:50:53 +0900

    install ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   roots: ['<rootDir>/unittests/'],
+  transform: { '^.+\\.(ts|tsx)$': 'ts-jest' },
   moduleNameMapper: { 'src/(.*)': '<rootDir>/src/$1' },
   moduleFileExtensions: ['ts', 'js'],
   verbose: true,
