5843f0fd1163fafab52e395490f1b1c52bd95593
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2022-12-30 13:21:59 +0900

    install storybook/addon-links

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   stories: ['../docs/**/*.stories.mdx', '../docs/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-essentials'],
+  addons: ['@storybook/addon-docs', '@storybook/addon-links'],
   framework: '@storybook/react',
   webpackFinal: async (config) => {
     config.resolve.alias = { ...config.resolve.alias, src: path.resolve(__dirname, '../src') };
