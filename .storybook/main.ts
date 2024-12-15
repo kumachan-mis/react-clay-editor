9785f6fcc441e043b91f1f27b74ce807e86e6de3
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2024-12-15 18:04:11 +0900

    .

---
+++ .storybook/main.ts
@@ -0,0 +1,37 @@
+import packageJson from '../package.json';
+
+import { StorybookConfig } from '@storybook/react-vite';
+// @ts-expect-error: commonjs module
+import remarkGfm from 'remark-gfm';
+
+const config: StorybookConfig = {
+  stories: [{ directory: '../docs' }],
+  staticDirs: ['../docs/public'],
+  addons: [
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        mdxPluginOptions: {
+          mdxCompileOptions: {
+            remarkPlugins: [remarkGfm],
+          },
+        },
+      },
+    },
+    '@storybook/addon-links',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  env: (config) => {
+    const katexVersion = packageJson.devDependencies.katex.slice(1);
+    const katexEnvSuffix = config?.NODE_ENV === 'production' ? '.min' : '';
+    const STORYBOOK_KATEX_CSS = `https://cdn.jsdelivr.net/npm/katex@${katexVersion}/dist/katex${katexEnvSuffix}.css`;
+    return { ...config, STORYBOOK_KATEX_CSS };
+  },
+  docs: {
+    autodocs: false,
+  },
+};
+export default config;
