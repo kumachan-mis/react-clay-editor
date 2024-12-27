d7e4a3729abf74a492514bae767366f57e97c318
Kumachan <29433058+kumachan-mis@users.noreply.github.com>
2024-12-27 17:10:00 +0900

    chore: fix storybook build (#443)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,8 +1,8 @@
 import packageJson from '../package.json';
 
 import { StorybookConfig } from '@storybook/react-vite';
-
-const remarkGfm = import('remark-gfm');
+// @ts-expect-error: commonjs module
+import remarkGfm from 'remark-gfm';
 
 const config: StorybookConfig = {
   stories: [{ directory: '../docs' }],
